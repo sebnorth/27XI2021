d86f0e4d3e2d7a0c046da73332142adf1c83c97d
sebnorth <sebnorth@gmail.com>
2021-11-27 15:40:16 +0100

    Dodaj pliki projektów.

---
+++ Modul3Zadanie10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul3Zadanie10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj wartość zmiennej ile: " );
+            short ile = Convert.ToInt16(Console.ReadLine());
+
+            for (int i = 0; i < ile; i++)
+            {
+                if (i != ile - 1)
+                {
+                    Console.Write("{0}, ", 7 * i);
+                }
+                else {
+                    Console.Write("{0}", 7 * i);
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
